4242d4c31fca3b3845752527dfa70270b888b577
alainfinck <alainf.@MacBook-Air-de-Alain.local>
2026-02-22 09:38:46 +0000

    Auto commit on 2026-02-22 09:38:46 UTC

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // Service Worker for CartePostale.cool PWA
 // CACHE_NAME is replaced at build time by scripts/generate-sw.mjs (do not edit manually).
-const CACHE_NAME = 'cartepostale-8e94cc2433d8'
+const CACHE_NAME = 'cartepostale-e3f43cfd2781'
 const STATIC_CACHE = [
   '/',
   '/editor',
