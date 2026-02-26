d9062d79808a457308794a5801bc1c2b3d78f6fc
Alain Finck <alain@wallprint.fr>
2026-02-26 11:05:00 +0100

    Auto commit on 2026-02-26 10:05:00 UTC

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // Service Worker for CartePostale.cool PWA
 // CACHE_NAME is replaced at build time by scripts/generate-sw.mjs (do not edit manually).
-const CACHE_NAME = 'cartepostale-0f4c1ac0f763'
+const CACHE_NAME = 'cartepostale-87e25da4e9b7'
 const STATIC_CACHE = [
   '/',
   '/editor',
